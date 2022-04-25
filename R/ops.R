0dbe9b06f40775176f5a8ce54cd08f249421d4c1
ivan <krylov.r00t@gmail.com>
2022-04-25 14:15:57 +0000

    Cache-tracking assignment: handle overwriting in various forms

--- R/ops.R
+++ R/ops.R
@@ -1,16 +1,30 @@
 `%<-%` <- function(lhs, rhs) {
 	symbol <- substitute(lhs)
-	stopifnot(is.symbol(symbol))
+	if (!is.symbol(symbol)) stop(
+		'Cache-tracking assignment only works on plain variable names'
+	)
 	expr   <- substitute(rhs)
 	frame  <- parent.frame()
 
 	fun <- function(assignment) {
-		if (!missing(assignment))
-			stop(deparse(substitute(assignment)))
-
-		do.cache(expr, frame, NULL, NULL)
+		if (missing(assignment)) {
+			do.cache(expr, frame, NULL, NULL)
+		} else {
+			warning(
+				'Overwriting a cache-tracked object (', symbol, ') ',
+				'with an untracked value', call. = FALSE
+			)
+			rm(list = as.character(symbol), envir = frame)
+			assign(as.character(symbol), assignment, envir = frame)
+		}
 	}
 
+	# makeActiveBinding stops if the variable already exists
+	if (exists(symbol, frame, inherits = FALSE))
+		rm(list = as.character(symbol), envir = frame)
 	makeActiveBinding(symbol, fun, frame)
 	invisible(fun())
 }
+
+`%->%` <- function(expr, symbol)
+	eval(substitute(symbol %<-% expr), parent.frame())
