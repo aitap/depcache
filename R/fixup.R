e0d65e636204b4969a9787d90a105b000c969098
aitap <krylov.r00t@gmail.com>
2022-05-03 17:29:27 +0000

    fixup: don't try to assign NULL to list elements

--- R/fixup.R
+++ R/fixup.R
@@ -9,7 +9,9 @@
 fixup <- function(x) {
 	# Source references can be different for equivalent functions and
 	# expressions and so must be removed.
-	if (is.language(x)) x <- removeSource(x)
+	# removeSource() only worked with functions until R 3.6.
+	if ((if (getRversion() >= '3.6') is.language else is.function)(x))
+		x <- removeSource(x)
 	# Functions are special in that they are recursive, but can't be
 	# directly subsetted: they consist of formals, body and environment,
 	# all of which could be subsetted.
@@ -23,9 +25,11 @@ fixup <- function(x) {
 	# The user might have to intervene here.
 	if (is.recursive(x) && !is.environment(x) && !is.function(x))
 		for (i in seq_along(x))
-			if (!.missing(x[[i]]))
-				# It's possible to stumble upon R_MissingArg inside
-				# formals(), better leave it alone
+			# 1. Inside formals() there can be R_MissingArg, better
+			# leave it alone.
+			# 2. There's nothing to fix up in a NULL, and inserting a
+			# NULL in a list removes the entry.
+			if (!.missing(x[[i]]) && !is.null(x[[i]]))
 				x[[i]] <- Recall(x[[i]])
 	# A character vector may be of native encoding, which may be
 	# different between operating systems, resulting in different byte
