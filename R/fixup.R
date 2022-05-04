884db9520f5ea8b2e2d36e233cff38eeeaf607e3
ivan <krylov.r00t@gmail.com>
2022-05-04 15:46:43 +0000

    fixup: use a more reliable removeSource on R < 3.6; test against strange values to be found while walking dependencies

--- R/fixup.R
+++ R/fixup.R
@@ -4,14 +4,43 @@
 	missing(x)
 }
 
+# In R < 3.6, removeSource only works with functions, despite language
+# objects also have source references to remove.
+# In R < 3.5, there's a bug concerning function calls involving NULLs:
+# elementwise replacement is done using x[[i]] <- Recall(x[[i]]), so
+# when x[[i]] is NULL, the language object ends up being shortened by
+# one.
+# On the one hand, we're playing with undocumented stuff here. On the
+# other hand, there will never be another version of R 3.x, so any
+# remaining bugs can be fixed right here.
+.removeSource <- function(x)
+	if (getRversion() >= '3.6') removeSource(x) else {
+		# no attributes for symbols; no source references for primitives
+		if (is.name(x) || is.primitive(x)) return(x)
+		attr(x, 'srcref') <- NULL
+		attr(x, 'wholeSrcref') <- NULL
+		attr(x, 'srcfile') <- NULL
+		# might contain more source references below
+		if (is.function(x)) {
+			# functions are recursive language objects, but aren't
+			# iterable
+			body(x) <- Recall(body(x))
+		} else if (is.recursive(x) && is.language(x)) {
+			for (i in seq_along(x))
+				x[i] <- list(Recall(x[[i]])) # watch out for NULLs
+		}
+		x
+	}
+
 # apply changes to objects in order to make their hashes reproducible
 # between R versions and operating systems
 fixup <- function(x) {
+	# There's nothing to fix up about primitives, and they trip up
+	# body() replacement.
+	if (is.primitive(x)) return(x)
 	# Source references can be different for equivalent functions and
 	# expressions and so must be removed.
-	# removeSource() only worked with functions until R 3.6.
-	if ((if (getRversion() >= '3.6') is.language else is.function)(x))
-		x <- removeSource(x)
+	if (is.language(x)) x <- .removeSource(x)
 	# Functions are special in that they are recursive, but can't be
 	# directly subsetted: they consist of formals, body and environment,
 	# all of which could be subsetted.
--- tests/fixup.R
+++ tests/fixup.R
@@ -1,5 +1,6 @@
 library(cacheR)
 
+# this is supposed to handle different "unknown" (native) encodings
 x <- '\xC5\xD8'
 Encoding(x) <- 'latin1'
 stopifnot(
@@ -7,15 +8,33 @@ stopifnot(
 	!identical(charToRaw(x), charToRaw(y)) # despite representation
 )
 
+check.all.equal <- function(x) for (n in names(x)) { message(n)
+	if (!isTRUE(ret <- all.equal(x[[n]], ff <- cacheR:::fixup(x[[n]]))))
+		stop(
+			sQuote(n), ' semantically changes after fixup\n',
+			'all.equal() says: ', ret, '\n',
+			'deparse(', n, '): ', paste(deparse(x[[n]]), collapse = '\n'), '\n',
+			'deparse(fixup(', n, ')): ', paste(deparse(ff), collapse = '\n'), '\n'
+		)
+}
+
 # This is a bit lazy, but does check a lot. Most of our fixes are for
 # language objects (which we have plenty of here) and recursive objects
 # (which functions consist of).
-e <- loadNamespace('cacheR')
-for (n in names(e))
-	if (!isTRUE(ret <- all.equal(e[[n]], cacheR:::fixup(e[[n]]))))
-		stop(
-			sQuote(n), ' semantically changes after fixup, ',
-			'all.equal() = ', ret
-		)
-
-# TODO: test as many kinds of *SXP as possible for all.equal
+check.all.equal(loadNamespace('cacheR'))
+# Try to cover all kinds of objects, except atomic vectors (we only
+# touch character vectors, covered above) and stuff we're unlikely to
+# ever meet in R code.
+check.all.equal(list(
+	NULL = NULL,
+	symbol = as.symbol('hello'),
+	pairlist = pairlist(a = 1),
+	closure = function() NULL,
+	environment = environment(),
+	language = quote(a + b),
+	special = substitute,
+	builtin = `+`,
+	expression = expression(haha),
+	list = list(NULL),
+	S4 = getClass('MethodDefinition')
+))
