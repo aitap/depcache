c51de201305579273b4c8e8d4033bf23a298d3a0
ivan <krylov.r00t@gmail.com>
2022-05-24 17:23:13 +0000

    cacheR.options: get defaults from multiple places, allowing them to override each other in a consistent manner

--- R/options.R
+++ R/options.R
@@ -1,8 +1,42 @@
 .cacheR.version <- packageVersion('cacheR')
 
+.defaults <- list(
+	'0.1' = list(
+		compress = TRUE,
+		local.only = FALSE,
+		format.version = 2
+	)
+)
+
 cacheR.options <- function(
+	defaults = getOption('cacheR.version', .cacheR.version),
 	dir = getOption('cacheR.dir', '.cache'),
-	compress = getOption('cacheR.compress', TRUE),
-	format.version = getOption('cacheR.format.version', 2),
-	skip = NULL, local.only = FALSE, version = .cacheR.version
-) NULL
+	skip = getOption('cacheR.skip', NULL),
+	compress, local.only, format.version
+) {
+	defaults <- .defaults[[match.arg(defaults, '0.1')]]
+
+	if (missing(compress)) compress <- getOption(
+		'cacheR.compress', defaults$compress
+	)
+	if (missing(local.only)) local.only <- getOption(
+		'cacheR.local.only', defaults$local.only
+	)
+	if (missing(format.version)) format.version <- getOption(
+		'cacheR.format.version',
+		defaults$format.version
+	)
+
+	stopifnot(
+		is.character(dir), length(dir) == 1L,
+		is.character(skip) || is.null(skip),
+		length(compress) == 1L && !is.na(compress),
+		is.logical(local.only) && length(local.only) == 1L,
+		format.version %in% c(2,3)
+	)
+
+	list(
+		dir = dir, compress = compress, skip = skip,
+		local.only = local.only, format.version = format.version
+	)
+}
